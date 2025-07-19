dcb84ccd97dd9e0a3091522044ccdfb20adcda87
rick <ricksanmateo27@gmail.com>
2025-07-19 15:27:05 +0800

    react frontend inputTodo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import React, { Fragment } from "react";
+
+// components
+import InputTodo from "./components/inputTodo";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="className">
+        <InputTodo />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
