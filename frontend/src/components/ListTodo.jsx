e0aad877ea450aabde46f43c0306f19ffdb35362
rick <ricksanmateo27@gmail.com>
2025-07-21 19:13:14 +0800

    added edit

--- frontend/src/components/ListTodo.jsx
+++ frontend/src/components/ListTodo.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 
+import EditTodo from "./EditTodo";
+
 const ListTodo = () => {
   const [todos, setTodos] = useState([]);
 
@@ -42,7 +44,9 @@ const ListTodo = () => {
           {todos.map((todo) => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
               <td>
                 <button
                   className="btn btn-danger"
